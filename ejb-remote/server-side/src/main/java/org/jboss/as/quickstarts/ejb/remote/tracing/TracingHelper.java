95f078bca623797ed9e676c35bff6beff071ecad
Miloslav Žežulka <mzezulka@localhost.localdomain>
2019-10-07 11:15:38 +0200

    Codec update.

--- ejb-remote/server-side/src/main/java/org/jboss/as/quickstarts/ejb/remote/tracing/TracingHelper.java
+++ ejb-remote/server-side/src/main/java/org/jboss/as/quickstarts/ejb/remote/tracing/TracingHelper.java
@@ -1,40 +1,42 @@
-package org.jboss.as.quickstarts.ejb.remote.startup;
+package org.jboss.as.quickstarts.ejb.remote.tracing;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-import javax.annotation.PostConstruct;
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
+import org.jboss.ejb.protocol.remote.tracing.SpanCodec;
+import org.jboss.ejb.protocol.remote.tracing.SpanFormat;
 
 import io.jaegertracing.Configuration;
 import io.jaegertracing.Configuration.ReporterConfiguration;
 import io.jaegertracing.Configuration.SamplerConfiguration;
 import io.jaegertracing.Configuration.SenderConfiguration;
-import io.opentracing.util.GlobalTracer;
+import io.jaegertracing.internal.JaegerTracer.Builder;
+import io.opentracing.Tracer;
 
-@Singleton
-@Startup
-public class StartupBean {
+public class TracingHelper {
 
     private static final String TRACER_CONFIG_LOCATION = "tracer_config.properties";
 
-    @PostConstruct
-    private void startMeUp() {
-        Properties config = loadConfig();
+    static Tracer getJaegerTracer(Properties config) {
         SamplerConfiguration samplerConfig = new SamplerConfiguration().withType("const").withParam(1);
         SenderConfiguration senderConfig = new SenderConfiguration()
                 .withAgentHost(config.getProperty("jaeger.reporter_host"))
                 .withAgentPort(Integer.decode(config.getProperty("jaeger.reporter_port")));
         ReporterConfiguration reporterConfig = new ReporterConfiguration().withLogSpans(true).withFlushInterval(1000)
                 .withMaxQueueSize(10000).withSender(senderConfig);
-        GlobalTracer.registerIfAbsent(new Configuration("tx-demo-participant").withSampler(samplerConfig)
-                .withReporter(reporterConfig).getTracer());
+        Builder bldr = new Configuration("EJB-REMOTE-SERVER")
+                .withSampler(samplerConfig)
+                .withReporter(reporterConfig)
+                .getTracerBuilder();
+        // We only need the extracting side since ejb server will not propagate any spans remotely, only
+        // accept them
+        bldr.registerExtractor(SpanFormat.EJB, new SpanCodec());
+        return bldr.build();
     }
 
-    private Properties loadConfig() {
-        try (InputStream fs = StartupBean.class.getClassLoader().getResourceAsStream(TRACER_CONFIG_LOCATION)) {
+    static Properties loadConfig() {
+        try (InputStream fs = TracingStartup.class.getClassLoader().getResourceAsStream(TRACER_CONFIG_LOCATION)) {
             Properties config = new Properties();
             config.load(fs);
             return config;
---
+++ ejb-remote/server-side/src/main/java/org/jboss/as/quickstarts/ejb/remote/tracing/TracingStartup.java
@@ -0,0 +1,21 @@
+package org.jboss.as.quickstarts.ejb.remote.tracing;
+
+import java.util.Properties;
+
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.annotation.PostConstruct;
+
+import io.opentracing.Tracer;
+import io.opentracing.util.GlobalTracer;
+
+@Singleton
+@Startup
+public class TracingStartup {
+    @PostConstruct
+    private void startTracing() {
+        Properties config = TracingHelper.loadConfig();
+        Tracer tracer = TracingHelper.getJaegerTracer(config);
+        GlobalTracer.registerIfAbsent(tracer);
+    }
+}
